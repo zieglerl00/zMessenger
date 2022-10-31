5b7c7c1e9d212c2eb843f58f23f0bf85c2be21a5
zieglerl00 <lukas.ziegler52@gmail.com>
2022-10-31 13:20:18 +0100

    WIP: #removing shopping item

--- api/urls.py
+++ api/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.get_shopping_list),
     path('add/', views.add_shopping_item),
-    path('delete/', views.remove_shopping_item),
+    path('delete/<int:pk>', views.remove_shopping_item),
 ]
